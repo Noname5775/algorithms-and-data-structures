b7dccfe996a536afccfca8410546d2e30d906a54
Noname5775 <martynenkodavid2307@gmail.com>
2026-04-16 09:06:29 +0500

    Time: 351 ms (93.15%) | Memory: 19.6 MB (94.43%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minCostConnectPoints(self, points):
+        n = len(points)
+        
+        visited = [False] * n
+        min_dist = [float('inf')] * n
+        min_dist[0] = 0
+
+        total_cost = 0
+
+        for _ in range(n):
+            u = -1
+
+            # ищем ближайшую точку
+            for i in range(n):
+                if not visited[i] and (u == -1 or min_dist[i] < min_dist[u]):
+                    u = i
+
+            visited[u] = True
+            total_cost += min_dist[u]
+
+            # обновляем расстояния
+            for v in range(n):
+                if not visited[v]:
+                    dist = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1])
+                    
+                    if dist < min_dist[v]:
+                        min_dist[v] = dist
+
+        return total_cost
